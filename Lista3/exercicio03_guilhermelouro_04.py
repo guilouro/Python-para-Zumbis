f8743bf3038c23edafe1e400b62ebb92a269f429
Guilherme Louro <guilherme-louro@hotmail.com>
2014-02-11 19:58:29 -0200

    Lista3 exercicio 04

---
+++ Lista3/exercicio03_guilhermelouro_04.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A seqüência de Fibonacci é a seguinte: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ... Sua regra de 
+formação é simples: os dois primeiros elementos são 1; a partir de então, cada elemento é a 
+soma dos dois anteriores. Faça um algoritmo que leia um número inteiro calcule o seu número 
+de Fibonacci. F1 = 1, F2 = 1, F3 = 2, etc. 
+"""
+fibo = [1,1]
+i = 0
+
+num = int(input("Entre com um número: "))
+
+while num > len(fibo):
+	fibo.append(fibo[i] + fibo[i+1])
+	i+=1
+
+print ('Fibonacci(%d): %d' %(num,fibo[num-1]))
